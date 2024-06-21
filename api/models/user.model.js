c5d2e59f3e180fc99c40e046b8dded6658d1ff75
Aditya Bhanu Garg <adityabhanugarg@gmail.com>
2024-06-22 01:25:34 +0530

    Created user Schema

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+}, {timestamps:true} // mongo db will automatically store the timestamps..
+)
+
+const User = mongoose.model('User', userSchema); // we create here user model!!
+
+export default User;
