302f93a8d494adbdf8f3522b264204755692312e
Aditya Bhanu Garg <adityabhanugarg@gmail.com>
2024-06-22 01:43:49 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -20,3 +21,9 @@ const port = 4000;
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`)
 })
+
+// app.get('/test',(req,res)=>{ //Test Api routes   // request is the data which we sends while response is the data which we receive... Here we created apiu route
+//             res.json({message:'Api is Working'});
+// })
+
+app.use('/api/user',userRoutes);
