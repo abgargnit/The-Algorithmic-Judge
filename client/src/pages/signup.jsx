e55ef9c1f5eac9eb98c7bdb6cbd47d3e957cb7ba
Aditya Bhanu Garg <adityabhanugarg@gmail.com>
2024-06-26 00:36:01 +0530

    connected signup frontend with backend

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,7 +1,28 @@
 import { Button, Label, TextInput } from 'flowbite-react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SignUp() {
+  // we will create a piece of state..
+  const [formData, setFormData] = useState({}); // formData is to sav the items and setForm Data to change it 
+  const handleChange = (e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value}) 
+  }
+  const onSubmitHandler = async (e) =>{
+    e.preventDefault(); // this will prevent the default behaviour of reloading the page...
+    // how will we submit it?
+    try {
+      const res = await fetch('/api/auth/signup',{
+      method:'POST',
+      headers:{ 'Content-Type':'application/json'},
+      body: JSON.stringify(formData),
+      })
+      // we added proxy at vite.config.js file for server
+      const data = await res.json();
+    } catch (error) {
+      
+    }
+  }
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -15,19 +36,19 @@ export default function SignUp() {
         </div>
         {/* right */}
 
-        <div className='flex-1'> 
+        <div className='flex-1' onSubmit={onSubmitHandler}> 
           <form className='flex flex-col gap-4'>
             <div>
               <Label value='Your username' />
-              <TextInput type='text' placeholder='Username' id='username' />
+              <TextInput type='text' placeholder='Username' id='username' onChange={handleChange}/>
             </div>
             <div>
               <Label value='Your email' />
-              <TextInput type='text' placeholder='Email' id='email' />
+              <TextInput type='email' placeholder='Email' id='email' onChange={handleChange}/>
             </div>
             <div>
               <Label value='Your password' />
-              <TextInput type='text' placeholder='Password' id='password' />
+              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
             </div>
             <Button color='gray' type='submit'>
               Sign Up
