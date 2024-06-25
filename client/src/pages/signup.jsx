86de33be3cfed03bb1a127e549c1315f31edeed1
Aditya Bhanu Garg <adityabhanugarg@gmail.com>
2024-06-26 00:01:52 +0530

    add signup frontend

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { Button, Label, TextInput } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
-const Signup = () => {
+export default function SignUp() {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+        <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-x; font-semibold dark:text-white'>
+            The
+            <span className='px-2 py-1 text-4xl'>Algorithmic</span>
+            Judge
+            </Link>
+        </div>
+        {/* right */}
 
-export default Signup
+        <div className='flex-1'> 
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Your username' />
+              <TextInput type='text' placeholder='Username' id='username' />
+            </div>
+            <div>
+              <Label value='Your email' />
+              <TextInput type='text' placeholder='Email' id='email' />
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput type='text' placeholder='Password' id='password' />
+            </div>
+            <Button color='gray' type='submit'>
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to='/sign-in' className='text-blue-500'>
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
